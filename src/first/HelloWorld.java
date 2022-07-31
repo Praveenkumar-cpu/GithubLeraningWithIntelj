b8c468f122d3832990efa034013c3f7dd507bfe0
praveen <praveenpkbadiger@gmail.com>
2022-07-31 20:10:37 +0530

    second commit after pull request

--- src/first/HelloWorld.java
+++ src/first/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args)
     {
         System.out.println("hi this is my first program");
-        System.out.println("hi here i changed the code");
+        System.out.println("hi ur changes correct check once");
     }
 }
